e43aaad3e4698542557d4349fc94dcc62286589d
Robot°16 16 <71988762+blak11@users.noreply.github.com>
2021-08-11 14:05:37 +0300

    Add files via upload

---
+++ Combo-maker-by-zed😂.py
@@ -0,0 +1,19 @@
+import random, sys, os, time
+os.system("rm -rf combo.txt")
+os.system("figlet Combo")
+print("=============================\n ba number\n========================\n bo nmuna 770, 750......htr          200k Combo")
+sk=input("saratakay: ")
+time.sleep(1)
+gg=1
+hh=":"
+def tt():
+    print(" \n   bosta 1daqa ean 2daqa\n\n")
+def haa():
+    for kar in range(200000):
+        dd=random.randint(1000000, 9999999)
+        sys.stdout = open("combo.txt", "a")
+        print(str(sk)+str(dd)+str(hh)+str(sk)+str(dd))
+        sys.stdout.flush()
+if gg == 1 :
+    tt()
+    haa()
